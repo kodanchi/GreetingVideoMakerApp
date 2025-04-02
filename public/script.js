da6a501cde869119c8f044f97209f8e644fe9c4c
Anas Alzahrani <answaza@hotmail.com>
2025-04-02 17:04:13 +0300

    fix ffmpeg-core url

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const { createFFmpeg, fetchFile } = FFmpeg;
 const ffmpeg = createFFmpeg({
   log: true,
-  corePath: 'https://unpkg.com/@ffmpeg/core@0.11.1/dist/ffmpeg-core.js',
+  corePath: 'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.11.1/dist/ffmpeg-core.js',
   wasmOptions: {
     env: {
       FF_USE_PTHREADS: false
